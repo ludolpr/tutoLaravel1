e9c016ccd56278cdfeb18a5b5adf8b9808cad0d4
ludolpr <ludolpr@gmail.com>
2024-05-28 09:02:29 +0200

    login ok

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\Models\Post;
-use App\Models\Role;
-use App\Models\Comment;
+// use App\Models\Post;
+// use App\Models\Role;
+// use App\Models\Comment;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -21,10 +21,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'pseudo',
+        'image',
         'email',
         'password',
     ];
-
+        
     /**
      * The attributes that should be hidden for serialization.
      *
